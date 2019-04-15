b6c9fea8ef5c9e54d8aa5293a60a1886b33db945
LuisAlbertoPerezDeLaCruz <luisalbertoperez@gmail.com>
2019-04-15 19:22:26 -0400

    Ajustes en urls del modulo de accounts

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,4 @@
-from django.conf.urls import url
-from django.contrib.auth import views as auth_views
-
 
 app_name = 'accounts'
 
-urlpatterns = [
-
-]
+urlpatterns = []
--- victorius/urls.py
+++ victorius/urls.py
@@ -19,6 +19,8 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from accounts import views as accounts_views
+from django.conf.urls import url
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
